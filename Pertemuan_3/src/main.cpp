0022fa755d4870786a0fd6396bb7cf9cd66a7825
Nadine Revandra Akila Putri <nadinerevandraakilaputri@mail.ugm.ac.id>
2026-09-15 16:39:38 +0700

    Pertemuan 3

---
+++ Pertemuan_3/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+const int potPin = 18; // potensiometer pwm input
+const int servoPin = 16;
+const int freq = 50;
+const int channel = 0;
+const int res = 12;
+
+void setup() {
+  ledcSetup(channel, freq, res);
+  ledcAttachPin(servoPin, channel);
+}
+
+void loop() {
+  int potValue = analogRead(potPin); // Hasil: 0 - 4095 pwm pwm
+  int pwmValue = map(potValue, 0, 4095, 0, 255); // Konversi ke 0 - 255 pwm 
+  
+  
+  ledcWrite(channel, pwmValue);
+  delay(15);
+}
+
